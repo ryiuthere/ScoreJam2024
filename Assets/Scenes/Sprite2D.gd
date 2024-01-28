22b8105b30e47f658c5a76df79baf67f532eff04
Thomasjnliu <thomasjnliu@gmail.com>
2024-01-27 21:00:23 -0500

    remove print

--- Assets/Scenes/Sprite2D.gd
+++ Assets/Scenes/Sprite2D.gd
@@ -7,5 +7,4 @@ func _physics_process(delta) -> void:
 	
 	# (sin(time * hover speed) * hover range) - offset
 	yPos = (sin(Time.get_ticks_msec() * 0.005) * 1) - 2
-	print(yPos)
 	%Sprite2D.position = Vector2(0,yPos)
