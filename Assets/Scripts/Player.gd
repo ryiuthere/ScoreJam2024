a33a99a461fdfd446e00cf983c38dbe7b90a440e
DashelBaldwin <dashelbaldwin@gmail.com>
2024-01-28 15:29:35 -0600

    UI + movement
    Added seconds timer, connected UI to game controller, tweaked movement constants, added fastfalling

--- Assets/Scripts/Player.gd
+++ Assets/Scripts/Player.gd
@@ -2,17 +2,19 @@ extends CharacterBody2D
 
 var MAX_SPEED := Vector2(200,300) # Maximum speed in x and y directions
 var ACCELERATION := 2000 # Direction for horizontal momentum
-var FRICTION := 900 # Horizontal drag when not moving
-var SLOW_FORCE := 2000 # Force for slowing down to max speed if going over
-var GRAVITY := 500 # Gravity
-var JUMP_FORCE := 15000 # Force applied when jumping
-var DASH_FORCE := Vector2(50000,25000) # Force applied when dashing in x or y direction
-var HOVER_FORCE := 900 # Force applied when hovering
-var HOVER_LENGTH := 0.7 # Length (in seconds) of hover time before touching groud
+var FRICTION := 1200 # Horizontal drag when not moving
+var SLOW_FORCE := 2200 # Force for slowing down to max speed if going over
+var GRAVITY := 620 # Gravity
+var JUMP_FORCE := 17500 # Force applied when jumping
+var DASH_FORCE := Vector2(60000,50000) # Force applied when dashing in x or y direction
+var HOVER_FORCE := 1850 # Force applied when hovering
+var HOVER_LENGTH := 60 # Length (in seconds) of hover time before touching groud
 var DASH_COOLDOWN := 1.0 # Max cooldown between dashes
 const DASH_COST := (1/6.8) # How much fuel dashing costs
-const FUEL_CONSUMPTION_RATE := 0.21 # Rate of fuel consumption
+const FUEL_CONSUMPTION_RATE := 0.075 # Rate of fuel consumption
 const MIN_DASH_FUEL_REQUIRED := (2.25/8.5) # Min fuel to dash
+const FASTFALL_FORCE := 1200 # Fastfall strength
+const FASTFALL_THRESHOLD := 250 # Maximum downwards velocity to fastfall
 
 @onready var axis := Vector2.ZERO # Input Axis
 @onready var dash_axis := Vector2.ZERO # Dash Axis
@@ -22,6 +24,7 @@ const MIN_DASH_FUEL_REQUIRED := (2.25/8.5) # Min fuel to dash
 @onready var can_hover : bool # Can player hover in current state?
 @onready var dash_cooldown : float # Cooldown between dashes
 @onready var fuel_amt := 1.0 # Current fuel amount
+@onready var fastfall : bool # Is the player holding down?
 
 const DashParticlesResource = preload("res://Assets/Scenes/DashParticles.tscn")
 
@@ -32,6 +35,7 @@ func get_input() -> void:
 	axis.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	jump_press = Input.is_action_just_pressed("jump")
 	jump_hold = Input.is_action_pressed("jump")
+	fastfall = Input.is_action_pressed("down")
 	dash_axis = Vector2.ZERO
 	if (Input.is_action_just_pressed("left")):
 		dash_axis.x -= 1 if $Timer.double_tap("left") else 0
@@ -81,6 +85,8 @@ func move(delta) -> void:
 		fuel_amt -= DASH_COST
 	elif (dash_cooldown > 0):
 		dash_cooldown -= delta
+	if (fastfall):
+		apply_fastfall(FASTFALL_FORCE * delta)
 	$HoverParticles.emitting = hovering
 	move_and_slide()
 
@@ -108,10 +114,14 @@ func apply_gravity(amount: float, delta: float) -> void:
 
 func apply_jump(amount: float) -> void:
 	velocity.y -= amount
+	
+func apply_fastfall(amount: float) -> void:
+	if velocity.y <= FASTFALL_THRESHOLD:
+		velocity.y += amount
 
 func apply_dash(dash_vector: Vector2) -> void:
 	velocity = Vector2(dash_vector.x * DASH_FORCE.x, dash_vector.y * DASH_FORCE.y)
 	self.add_child(DashParticlesResource.instantiate())
 
 func refill_fuel(amount: float) -> void:
-	fuel_amt = clamp(fuel_amt + amount, 0, 1)
+	fuel_amt = clamp(fuel_amt + amount , 0, 1)
--- Assets/Scripts/game_controller.gd
+++ Assets/Scripts/game_controller.gd
@@ -1,17 +1,34 @@
 extends Node
 
 var target_goal := "right"
-@export var score := 0
-@export var score_increment := 1000
+var score := 0
+var time := 300
+
+@export var delivery_score := 2500
+@export var coin_pickup_score := 100
+
+@export var timer : Timer
+
+func _ready():
+	timer = $GameTimer
 
 func touch_goal(goal: String):
 	if target_goal == goal:
 		target_goal = "right" if goal == "left" else "left"
-		score += score_increment
-		$TileMap.randomize_tileset()
+		score += delivery_score
+		$TileMap.randomize_tileset() # This causes a lagspike, is it possible to run this concurrently during gameplay?
+		$Player.refill_fuel(1/3.4)
 
 func _on_goal_1_body_entered(body):
 	touch_goal("left")
 
 func _on_goal_2_body_entered(body):
 	touch_goal("right")
+
+func _on_coin_pickup_body_entered(body): # Connect this to a coin emitter 
+	score += coin_pickup_score
+
+func _on_game_timer_timeout(): # Called every second
+	time -= 1
+	if time <= 0:
+		print("debug game over") # TODO: remove
